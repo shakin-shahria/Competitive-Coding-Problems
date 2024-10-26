3e4f3d0987bc560a63383f89a4582ff899712bb5
shakin-shahria <shakinshaharia@gmail.com>
2024-10-26 12:50:59 +0600

    Simple arithmetic operations-taking input from the user

--- code6.cpp
+++ code6.cpp
@@ -4,3 +4,64 @@
 // 2 for performing subtraction
 // 3 for performing multiplication
 //  4 for performing  division.
+
+#include <iostream>
+using namespace std;
+int main(){
+
+ int n1,n2,choice,result;
+
+ cout<<"Enter Your first number: ";
+ cin>> n1;
+
+ cout<<"Enter Your second number: ";
+ cin>> n2;
+
+ cout<<"Enter Yours choice: "<<endl;
+ cout<< "1.Addition"<<endl;
+ cout<< "2.Subtraction"<<endl;
+ cout<< "3.Multiplication"<<endl;
+ cout<< "4.Division"<<endl;
+ cin>> choice;
+
+ if (choice ==1){
+
+    result = n1+n2;
+    cout<<n1<<" + "<<n2<<" is "<<result<<endl;
+
+ }
+
+else if (choice ==2){
+
+    result = n1-n2;
+    cout<<n1<<" - "<<n2<<" is "<<result<<endl;
+
+ }
+
+ else if (choice ==3){
+
+    result = n1*n2;
+    cout<<n1<<" * "<<n2<<" is "<<result<<endl;
+
+ }
+
+ else if (choice ==4){
+
+    result = n1/n2;
+    cout<<n1<<" / "<<n2<<" is "<<result<<endl;
+
+ }
+
+ else{
+    cout<<"Wrong input"<<endl;
+ }
+ 
+
+
+
+
+
+
+
+
+}
