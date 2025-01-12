1abb11b3e5764a1402e72bb23461d0ba7fb3547c
shakin-shahria <shakinshaharia@gmail.com>
2025-01-12 17:07:34 +0600

    Caltualtion the number of thives and their profits

--- Number_of_Thives_Fractional_Knapsack.cpp
+++ Number_of_Thives_Fractional_Knapsack.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
-//#include<algorithm>
+#include<algorithm>
+
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -8,26 +9,19 @@ struct Item
     int item_number;
     double weight;
     double value;
-    double value_weight_ratio;
 };
 
-//typedef struct Item Item;
+typedef struct Item Item;
 
 bool compare(Item &left, Item &right)
 {
-    if(left.value_weight_ratio>right.value_weight_ratio) return true;
+    if(left.value * right.weight > left.weight * right.value) return true;
     else return false;
 }
 
 
 double fractional_knapsack(Item items[], int n, int capacity)
 {
-    /// step 1: Find the value-weight ratio for each item
-
-    for(int i=0; i<n; i++)
-    {
-        items[i].value_weight_ratio = items[i].value/items[i].weight;
-    }
 
     /// step 2: Sort the items based on value-weight ratio (descending order)
     sort(items, items+n, compare);
@@ -39,18 +33,29 @@ double fractional_knapsack(Item items[], int n, int capacity)
     cout << "Picked Items: ";
     for(int i=0; i<n; i++)
     {
+        if(items[i].value==0 && items[i].weight==0)
+            continue;
+
         if(capacity>=items[i].weight)
         {
             capacity -= items[i].weight;
             profit += items[i].value;
 
-
+            items[i].weight = 0;
+            items[i].value = 0;
 
             cout << items[i].item_number << " ";
         }
         else
         {
-            profit += (capacity * items[i].value_weight_ratio);
+            profit += (capacity * (items[i].value/items[i].weight));
+
+            items[i].value -= (capacity * (items[i].value/items[i].weight));
+
+            items[i].weight -= capacity;
+
+
+
 
             capacity = 0;
 
@@ -65,23 +70,50 @@ double fractional_knapsack(Item items[], int n, int capacity)
     return profit;
 }
 
+
+void printItems(Item items[], int n)
+{
+    cout << endl;
+    for(int i=0; i<n; i++)
+    {
+        cout << items[i].item_number << " " << items[i].value << " " << items[i].weight << endl;
+    }
+    cout << endl;
+}
+
 int main()
 {
 
     int n = 6;
     Item items[n] = {
-        {1, 10, 60, 0},
-        {2, 20, 100, 0},
-        {3, 30, 120, 0},
-        {4, 40, 80, 0},
-        {5, 50, 90, 0},
-        {6, 60, 150, 0}
+        {1, 10, 60},
+        {2, 20, 100},
+        {3, 30, 120},
+        {4, 40, 80},
+        {5, 50, 90},
+        {6, 60, 150}
     };
     int capacity = 50;
 
-
+    cout << "Thief 1: \n";
     double result = fractional_knapsack(items, n, capacity);
     cout << "Total Profit: " << result << endl;
 
+    printItems(items, n);
+
+    int thief = 2;
+
+    while(result!=0)
+    {
+        cout << "Thief " << thief << " : \n";
+        result = fractional_knapsack(items, n, capacity);
+        cout << "Total Profit: " << result << endl;
+
+        printItems(items, n);
+
+        thief++;
+
+    }
     return 0;
 }
+
